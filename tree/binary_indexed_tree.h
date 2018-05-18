81b26544cd8891afea07013464e436488dcaa9f5
gogdizzy <gogdizzy@qq.com>
2018-05-18 18:54:56 +0800

    add

---
+++ tree/binary_indexed_tree.h
@@ -0,0 +1,25 @@
+//
+// Created by 关鑫 on 2018/5/18.
+//
+
+#pragma once
+
+#include <functional>
+
+#include <util/bitop.h>
+
+using std::function;
+
+namespace better {
+
+class binary_indexed_tree {
+
+public:
+    virtual
+
+};
+
+}
+
+
+
---
+++ util/bitop.h
@@ -0,0 +1,16 @@
+//
+// Created by 关鑫 on 2018/5/18.
+//
+
+#pragma once
+
+namespace better {
+
+namespace bitop {
+
+inline int lowbit( int x ) { return x & -x; }
+
+}
+
+}
+
