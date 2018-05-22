19280ec44820255b1191646d8499a1b2e0abbdb1
gogdizzy <gogdizzy@qq.com>
2018-05-22 18:04:47 +0800

    add bit

--- test/test_binary_indexed_tree.cpp
+++ test/test_binary_indexed_tree.cpp
@@ -3,6 +3,7 @@
 //
 
 #include <stdio.h>
+#include <stdint.h>
 #include <tree/binary_indexed_tree.h>
 
 template < typename T >
@@ -16,6 +17,18 @@ using bit_1drmrq = better::binary_indexed_tree_1drmrq< T >;
 
 template < typename T >
 using bit_1dmin = better::binary_indexed_tree_1dmin< T >;
+
+template < typename T >
+using bit_2d1mrq = better::binary_indexed_tree_2d1mrq< T >;
+
+template < typename T >
+using bit_2drm1q = better::binary_indexed_tree_2drm1q< T >;
+
+template < typename T >
+using bit_2drmrq = better::binary_indexed_tree_2drmrq< T >;
+
+template < typename T = int >
+using bit_1dkth = better::binary_indexed_tree_1dkth< T >;
 
 int main() {
 
@@ -31,17 +44,19 @@ int main() {
     printf( "%d\n", bit1.query( 2, 3 ) );
     printf( "%d\n", bit1.query( 1, 2 ) );
 
+
+
     bit_1drm1q< int > bit2( 64 );
-
     bit2.modify( 1, 64, 100 );
-
     printf( "%d\n", bit2.query( 50 ) );
 
+
+
     bit_1drmrq< int > bit3( 64 );
-
     bit3.modify( 1, 10, 5 );
-
     printf( "%d\n", bit3.query( 1, 5 ) );
+
+
 
     bit_1dmin< int > bit4( 64, std::greater< int >() );
 
@@ -50,6 +65,35 @@ int main() {
     bit4.modify( 3, 2 );
 
     printf( "%d\n", bit4.query( 2, 3 ) );
+
+
+
+    bit_2d1mrq< int > bit5( 64, 64 );
+
+    bit5.modify( 10, 10, 1 );
+    bit5.modify( 9, 9, 1 );
+
+    printf( "%d\n", bit5.query( 9, 9, 10, 10 ) );
+
+
+    bit_2drm1q< int > bit6( 64, 64 );
+
+    bit6.modify( 1, 1, 10, 10, 1 );
+    printf( "%d\n", bit6.query( 5, 5 ) );
+
+    bit_2drmrq< int64_t > bit7( 64, 64 );
+
+    bit7.modify( 1, 1, 10, 10, 100000000L );
+    printf( "%lld\n", bit7.query( 1, 1, 10, 10 ) );
+
+
+    bit_1dkth<> bit8( 64 );
+
+    bit8.modify( 3, 5 );
+    bit8.modify( 3, -4 );
+    bit8.modify( 7, 5 );
+
+    printf( "%d\n", bit8.query( 6 ) );
 
     return 0;
 }
--- tree/binary_indexed_tree.h
+++ tree/binary_indexed_tree.h
@@ -3,6 +3,8 @@
 // 使用须知：位置0是作为boundary的，所以查询和更新最小位置是1
 // 命名规则：binary_indexed_tree_[x]d[y]m[z]q，d表示维度，m表示修改，q表示查询，1表示点，r表示范围
 //         例如，binary_indexed_tree_1d1mrq表示一维，点修改，范围查询的树状数组
+//         特例，binary_indexed_tree_1dmin表示一维，点修改，范围查询最值
+//         特例，binary_indexed_tree_1dkth表示一维，点修改，查询第k小
 
 #pragma once
 
@@ -69,12 +71,10 @@ class binary_indexed_tree_1drm1q {
 public:
     binary_indexed_tree_1drm1q( int maxlen,
                                 std::function< T( T, T ) > add = std::plus< T >(),
-                                std::function< T( T, T ) > sub = std::minus< T >(),
                                 std::function< T( T ) > neg = std::negate< T >() ) {
         this->maxlen = maxlen;
         bit_b = new T[maxlen + 1];
         this->add = add;
-        this->sub = sub;
         this->neg = neg;
     }
 
@@ -112,7 +112,6 @@ private:
     int maxlen;
     T *bit_b;
     std::function< T( T, T ) > add;
-    std::function< T( T, T ) > sub;
     std::function< T( T ) > neg;
 };
 
@@ -124,10 +123,10 @@ public:
     binary_indexed_tree_1drmrq( int maxlen,
                                 std::function< T( T, T ) > add = std::plus< T >(),
                                 std::function< T( T, T ) > sub = std::minus< T >(),
-                                std::function< T( T ) > neg = std::negate< T >(),
-                                std::function< T( T, int ) > mul = std::multiplies< T >() ) {
+                                std::function< T( T, int ) > mul = std::multiplies< T >(),
+                                std::function< T( T ) > neg = std::negate< T >() ) {
         this->maxlen = maxlen;
-        bit_b = new binary_indexed_tree_1drm1q< T >( maxlen, add, sub, neg );
+        bit_b = new binary_indexed_tree_1drm1q< T >( maxlen, add, neg );
         bit_c = new binary_indexed_tree_1d1mrq< T >( maxlen, add, sub );
         this->add = add;
         this->sub = sub;
@@ -141,6 +140,7 @@ public:
     }
 
     inline void modify( int x, T v ) {
+        if( x <= 0 ) return;
         bit_b->modify( x, v );
         bit_c->modify( x, mul( v, x ) );
     }
@@ -171,8 +171,8 @@ private:
     binary_indexed_tree_1d1mrq< T > *bit_c;
     std::function< T( T, T ) > add;
     std::function< T( T, T ) > sub;
-    std::function< T( T ) > neg;
     std::function< T( T, int ) > mul;
+    std::function< T( T ) > neg;
 };
 
 
@@ -201,7 +201,8 @@ public:
                 if( cmp( v, m[i] ) ) m[i] = v;
                 else break;
             }
-        } else {
+        }
+        else {
             T oldv = a[p];
             a[p] = v;
             // 之前的最值不是a[p]，现在a[p]更大，更不可能是最值
@@ -236,7 +237,8 @@ public:
             if( r - lbr >= l ) {
                 if( cmp( m[r], ans ) ) ans = m[r];
                 r -= lbr;
-            } else {
+            }
+            else {
                 if( cmp( a[r], ans ) ) ans = a[r];
                 --r;
             }
@@ -253,6 +255,259 @@ private:
     T *a;
     T *m;
     std::function< bool( T, T ) > cmp;
+};
+
+
+// 目前只支持固定数据种类，修改数据个数的模式
+// 用户需要自己先将数据离散化，找到每个数据对应的排位
+// 本数据结构只能处理排位数量更新，数据最大位置不超过2^30
+template< typename T = int >
+class binary_indexed_tree_1dkth {
+
+public:
+    binary_indexed_tree_1dkth( int maxlen ) {
+        this->maxlen = maxlen;
+        this->total = 0;
+        bit_a = new T[maxlen + 1];
+    }
+
+    virtual ~binary_indexed_tree_1dkth() {
+        if( bit_a != nullptr ) delete[] bit_a;
+    }
+
+    void modify( int x, int v ) {
+        total += v;
+        for( ; 0 < x && x <= maxlen; x += lowbit( x ) ) {
+            bit_a[x] += v;
+        }
+    }
+
+    int query( int kth ) {
+        if( kth <= 0 || kth > total ) return -1;
+        int p = 0;
+        for( int i = 30; i >= 0; --i ) {
+            int len = ( 1 << i );
+            if( p + len <= maxlen && bit_a[p + len] < kth ) {
+                kth -= bit_a[p + len];
+                p += len;
+            }
+        }
+        return p + 1;
+    }
+
+private:
+    // 基于1，所以注意数组边界
+    // a[i]第i个数字出现次数
+    // bit_a就是在a的基础上做树状数组
+    // 寻找第k小，就是一个逐渐逼近的过程，因为bit_a[i]表示了自己控制范围内的数字个数，
+    // 所以先找左边最接近kth的2^x，然后依次缩小步长，找x右边最大的能逼近kth的2的幂
+
+    int maxlen;
+    T total;
+    T *bit_a;
+};
+
+
+template < typename T >
+class binary_indexed_tree_2d1mrq {
+
+public:
+    binary_indexed_tree_2d1mrq( int xmaxlen, int ymaxlen,
+                                std::function< T( T, T ) > add = std::plus< T >(),
+                                std::function< T( T, T ) > sub = std::minus< T >() ) {
+        this->xmaxlen = xmaxlen;
+        this->ymaxlen = ymaxlen;
+        data = new T[( xmaxlen + 1 ) * ( ymaxlen + 1 )];
+        for( int i = 0; i < ( xmaxlen + 1 ) * ( ymaxlen + 1 ); ++i ) data[i] = T();
+        bit_a = new T *[ymaxlen + 1];
+        for( int i = 0; i <= ymaxlen; ++i ) bit_a[i] = data + i * ( xmaxlen + 1 );
+        this->add = add;
+        this->sub = sub;
+    }
+
+    virtual ~binary_indexed_tree_2d1mrq() {
+        if( bit_a != nullptr ) delete[] bit_a;
+        if( data != nullptr ) delete[] data;
+    }
+
+    inline void modify( int x, int y, T v ) {
+        if( x <= 0 || y <= 0 ) return;
+        for( int j = y; 0 < j && j <= ymaxlen; j += lowbit( j ) ) {
+            for( int i = x; 0 < i && i <= xmaxlen; i += lowbit( i ) ) {
+                bit_a[j][i] = add( bit_a[j][i], v );
+            }
+        }
+    }
+
+    inline T query( int x, int y ) {
+        T s = T();
+        for( int j = y > ymaxlen ? ymaxlen : y; j > 0; j -= lowbit( j ) ) {
+            for( int i = x > xmaxlen ? xmaxlen : x; i > 0; i -= lowbit( i ) ) {
+                s = add( s, bit_a[j][i] );
+            }
+        }
+        return s;
+    }
+
+    inline T query( int x1, int y1, int x2, int y2 ) {
+        return sub( add( query( x2, y2 ), query( x1 - 1, y1 - 1 ) ), add( query( x2, y1 - 1 ), query( x1 - 1, y2 ) ) );
+    }
+
+private:
+    // 基于1，所以注意数组边界
+    // a[i][j]表示原来数组的值
+    // bit_a就是在a的基础上做的2维树状数组
+
+    int xmaxlen;
+    int ymaxlen;
+    T **bit_a;
+    T *data;
+    std::function< T( T, T ) > add;
+    std::function< T( T, T ) > sub;
+};
+
+
+template < typename T >
+class binary_indexed_tree_2drm1q {
+
+public:
+    binary_indexed_tree_2drm1q( int xmaxlen, int ymaxlen,
+                                std::function< T( T, T ) > add = std::plus< T >(),
+                                std::function< T( T ) > neg = std::negate< T >() ) {
+        this->xmaxlen = xmaxlen;
+        this->ymaxlen = ymaxlen;
+        data = new T[( xmaxlen + 1 ) * ( ymaxlen + 1 )];
+        for( int i = 0; i < ( xmaxlen + 1 ) * ( ymaxlen + 1 ); ++i ) data[i] = T();
+        bit_b = new T *[ymaxlen + 1];
+        for( int i = 0; i <= ymaxlen; ++i ) bit_b[i] = data + i * ( xmaxlen + 1 );
+        this->add = add;
+        this->neg = neg;
+    }
+
+    virtual ~binary_indexed_tree_2drm1q() {
+        if( bit_b != nullptr ) delete[] bit_b;
+        if( data != nullptr ) delete[] data;
+    }
+
+
+    inline void modify( int x, int y, T v ) {
+        if( x <= 0 || y <= 0 ) return;
+        for( int j = y > ymaxlen ? ymaxlen : y; j > 0; j -= lowbit( j ) ) {
+            for( int i = x > xmaxlen ? xmaxlen : x; i > 0; i -= lowbit( i ) ) {
+                bit_b[j][i] = add( bit_b[j][i], v );
+            }
+        }
+    }
+
+    inline void modify( int x1, int y1, int x2, int y2, T v ) {
+        modify( x2, y2, v );
+        modify( x2, y1 - 1, neg( v ) );
+        modify( x1 - 1, y2, neg( v ) );
+        modify( x1 - 1, y1 - 1, v );
+    }
+
+    inline T query( int x, int y ) {
+        T s = T();
+        for( int j = y; 0 < j && j <= ymaxlen; j += lowbit( j ) ) {
+            for( int i = x; 0 < i && i <= xmaxlen; i += lowbit( i ) ) {
+                s = add( s, bit_b[j][i] );
+            }
+        }
+        return s;
+    }
+
+private:
+    // 基于1，所以注意数组边界
+    // a[i][j]表示原来数组的值
+    // b[i][j]表示a[1..i][1..j]这个区域每个点被累计了多少值
+    // bit_b就是在b的基础上做的2维树状数组
+
+    int xmaxlen;
+    int ymaxlen;
+    T **bit_b;
+    T *data;
+    std::function< T( T, T ) > add;
+    std::function< T( T ) > neg;
+};
+
+
+template < typename T >
+class binary_indexed_tree_2drmrq {
+
+public:
+    binary_indexed_tree_2drmrq( int xmaxlen, int ymaxlen,
+                                std::function< T( T, T ) > add = std::plus< T >(),
+                                std::function< T( T, T ) > sub = std::minus< T >(),
+                                std::function< T( T, int ) > mul = std::multiplies< T >(),
+                                std::function< T( T ) > neg = std::negate< T >() ) {
+        this->xmaxlen = xmaxlen;
+        this->ymaxlen = ymaxlen;
+        bit_b = new binary_indexed_tree_2d1mrq< T >( xmaxlen, ymaxlen, add, sub );
+        bit_c = new binary_indexed_tree_2d1mrq< T >( xmaxlen, ymaxlen, add, sub );
+        bit_d = new binary_indexed_tree_2d1mrq< T >( xmaxlen, ymaxlen, add, sub );
+        bit_e = new binary_indexed_tree_2d1mrq< T >( xmaxlen, ymaxlen, add, sub );
+        this->add = add;
+        this->sub = sub;
+        this->mul = mul;
+        this->neg = neg;
+    }
+
+    virtual ~binary_indexed_tree_2drmrq() {
+        if( bit_b != nullptr ) delete bit_b;
+        if( bit_c != nullptr ) delete bit_c;
+        if( bit_d != nullptr ) delete bit_d;
+        if( bit_e != nullptr ) delete bit_e;
+    }
+
+    inline void modify( int x, int y, T v ) {
+        if( x <= 0 || y <= 0 ) return;
+        bit_b->modify( x, y, v ), bit_b->modify( 1, y, neg( v ) ), bit_b->modify( x, 1, neg( v ) ),
+                bit_b->modify( 1, 1, v );
+        bit_c->modify( x, y, neg( mul( v, x ) ) ), bit_c->modify( x, 1, mul( v, x ) );
+        bit_d->modify( x, y, neg( mul( v, y ) ) ), bit_d->modify( 1, y, mul( v, y ) );
+        bit_e->modify( x, y, mul( v, x * y ) );
+    }
+
+    inline void modify( int x1, int y1, int x2, int y2, T v ) {
+        modify( x2, y2, v );
+        modify( x2, y1 - 1, neg( v ) );
+        modify( x1 - 1, y2, neg( v ) );
+        modify( x1 - 1, y1 - 1, v );
+    }
+
+    inline T query( int x, int y ) {
+        if( x <= 0 || y <= 0 ) return T();
+        return add( add( mul( bit_b->query( x, y ), x * y ), mul( bit_c->query( x, y ), y ) ),
+                    add( mul( bit_d->query( x, y ), x ), bit_e->query( x, y ) )
+        );
+    }
+
+    inline T query( int x1, int y1, int x2, int y2 ) {
+        return sub( add( query( x2, y2 ), query( x1 - 1, y1 - 1 ) ),
+                    add( query( x2, y1 - 1 ), query( x1 - 1, y2 ) )
+        );
+    }
+
+private:
+    // 参考http://hi.baidu.com/strongoier/item/13f22104f6b4c50d6c904852
+    // 我日，太妙了
+    // 基于1，所以注意数组边界
+    // 把修改简记为(x', y')，加的数为d，询问简记为(x, y)
+    // bit_b 当x' > x, y' > y时，(x', y')对(x, y)的贡献值为x * y * d，因此用树状数组维护所有(x, y)的sigma{d}
+    // bit_c 当x' <= x, y' > y时，(x', y')对(x, y)的贡献值为x' * y * d，因此用树状数组维护所有(x, y)的sigma{x' * d}
+    // bit_d 当x' > x, y' <= y时，(x', y')对(x, y)的贡献值为x * y' * d，因此用树状数组维护所有(x, y)的sigma{y' * d}
+    // bit_e 当x' <= x, y' <= y时，(x', y')对(x, y)的贡献值为x' * y' * d，因此用树状数组维护所有(x, y)的sigma{x' * y' * d}
+
+    int xmaxlen;
+    int ymaxlen;
+    binary_indexed_tree_2d1mrq< T > *bit_b;
+    binary_indexed_tree_2d1mrq< T > *bit_c;
+    binary_indexed_tree_2d1mrq< T > *bit_d;
+    binary_indexed_tree_2d1mrq< T > *bit_e;
+    std::function< T( T, T ) > add;
+    std::function< T( T, T ) > sub;
+    std::function< T( T, int ) > mul;
+    std::function< T( T ) > neg;
 };
 
 
